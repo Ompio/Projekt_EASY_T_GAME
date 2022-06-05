76b1edf5bcc3090e546c921573a4c39b3b40ba81
Ompio <72317376+Ompio@users.noreply.github.com>
2022-06-05 18:26:15 +0200

    Porządki 3

--- proj_test/proj_test/f_sfml.cpp
+++ proj_test/proj_test/f_sfml.cpp
@@ -4,15 +4,8 @@ bool isSpriteHover(sf::FloatRect sprite, sf::RenderWindow& window)
     sf::Vector2f mp;
     mp.x = sf::Mouse::getPosition(window).x;
     mp.y = sf::Mouse::getPosition(window).y;
-<<<<<<< HEAD:proj_test/proj_test/f_sfml.cpp
     if (sprite.contains(mp)) {
         return true;
-=======
-    //std::cout << mp.x << ":X | Y:" << mp.y << std::endl;
-    if (sprite.contains(mp)) {
-        return true;
-        //std::cout << "beka" << std::endl;
->>>>>>> cebeb1624b917a18b8b5045b5eb6ef2c578cd745:GraTurowa/GraTurowa/f_sfml.cpp
     }
     return false;
 }
